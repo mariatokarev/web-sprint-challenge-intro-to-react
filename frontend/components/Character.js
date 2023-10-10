1910da3de21ba3b3802595a6f31400a3f2ee09f7
mariatokarev <144080022+mariatokarev@users.noreply.github.com>
2023-10-10 13:45:46 -0400

    Add files via upload

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -6,52 +6,54 @@ function Character(props) { // ❗ Add the props
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   const [showHomeworld, setShowHomeworld] = useState(false);
   const [planetData, setPlanetData] = useState([]);
+
+  const toggleHomeworld = () => {
+    if (!showHomeworld) {
+
+      fetchPlanetData([]);
+    }
+   
+    setShowHomeworld(!showHomeworld);
+  };
+
   useEffect(() => {
     if (showHomeworld) {
       fetchPlanetData();
     }
   }, [showHomeworld]);
 
-
-  const toggleHomeworld = () => {
-    setShowHomeworld(!showHomeworld);
-  };
-
   const fetchPlanetData = () => {
     fetch('http://localhost:9009/api/planets')
       .then((response) => response.json())
       .then((planets) => {
-        console.log(planets)
-
-        setPlanetData(planets[0].name);
-        console.log(planetData)
-    //     if (Array.isArray(data) && data.length > 0) {
-         
-    //      } else {
-    //     setPlanetData('No planet data available');
-    // }
+        if (planets.length > 0) {
+          setPlanetData(planets[0].name);
+        } else {
+          setPlanetData('No planet data available');
+        }
       })
       .catch((error) => {
         console.error('Error fetching planet data:', error);
-       });
-  }
-
+      });
+  };
+ 
   return (
     <div className="character-card">
       <h3
-        className={`character-name ${showHomeworld ? 'active' : ' '}`}
+        className={`character-name ${showHomeworld ? 'active' : ''}`}
         onClick={toggleHomeworld}
       >
         {props.data.name}
       </h3>
-
-      {showHomeworld && (
         <p>
-        Planet:
-         <span className='character-planet'>{planetData ? planetData.name :'Loading'}</span>
-           {/* Use the same markup with the same attributes as in the mock */}
+          Planet:
+          <span className={`character-planet ${planetData ? planetData : 'Loading'}`}
+          >
+            {planetData}
+          </span>
+
         </p>
-      )}
+     
     </div>
   );
 }
