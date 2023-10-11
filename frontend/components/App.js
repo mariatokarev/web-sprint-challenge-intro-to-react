018cd46d583f8d1f233b55f9d22223aa2dff3e20
mariatokarev <144080022+mariatokarev@users.noreply.github.com>
2023-10-11 13:33:27 -0400

    Add files via upload

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,10 +6,10 @@ import Character from './Character'
 const urlPlanets = 'http://localhost:9009/api/planets';
 const urlPeople = 'http://localhost:9009/api/people';
 
-function App() {
+ function App() {
   const [peopleData, setPeopleData] = useState([]);
   const [planetsData, setPlanetsData] = useState([]);
-  
+  const [showHomeworld, setShowHomeworld] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -26,21 +26,34 @@ function App() {
     fetchData();
   }, []);
 
-  if (peopleData.length === 0 || planetsData.length === 0) {
-    return 'Loading...';
-  }
+  const openHomeworld = (urlPlanets) => {
+    setShowHomeworld(urlPlanets);
+  };
+
+  const closeHomeworld = () => {
+    setShowHomeworld(null);
+  };
 
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {peopleData.map((person, index, planetsData) => (
-        <Character key={index} data={person} planet={planetsData[index].urlPlanets} />
+      {peopleData.map((person, index) => (
+        <div key={index} className="character-card">
+          <h3 className="character-name">{person.name}</h3>
+          <button onClick={() => openHomeworld(person.homeworld)}>
+            {person.name}'s Homeworld
+          </button>
+          {showHomeworld === person.homeworld && (
+            <div className="character-planet">
+            
+            </div>
+          )}
+        </div>
       ))}
     </div>
   );
 }
-  
 export default App
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
