f7270edbd1eb15863dd611ec307c3ddbf096955f
mariatokarev <144080022+mariatokarev@users.noreply.github.com>
2023-10-11 11:41:07 -0400

    Add files via upload

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -5,38 +5,35 @@ function Character(props) {
     const [showHomeworld, setShowHomeworld] = useState(false);
     const [planetData, setPlanetData] = useState(null);
     
-      };
-      const toggleHomeworld = () => {
-        setShowHomeworld(!showHomeworld);
-      }
-        
-    
+      
     useEffect(() => {
         if (showHomeworld) {
-            fetchPlanetData();
-          }
-        }, [showHomeworld]);
-        
-      
-       
+          fetchPlanetData();
+        }
+      }, [showHomeworld]);
+    
+      const toggleHomeworld = () => {
+        setShowHomeworld(!showHomeworld);
+      };
     
       return (
         <div className="character-card">
       <h3
-        className={`character-name ${showHomeworld ? 'active' : ''}`}
-        onClick={toggleHomeworld}
+        onClick={toggleHomeworld} className={`character-name ${showHomeworld ? 'active' : ''}`}
+       
       >
         {props.data.name}
       </h3>
       <p>
         Planet:
-        <span className={`character-planet ${planetData ? '' : 'loading'}`}>
-          {planetData ? planetData : 'Loading'}
+        <span className={`character-planet ${planetData ? '' : 'Loading...'}`}>
+           
         </span>
       </p>
     </div>
   );
-
+      }
+    
   
     
 export default Character
