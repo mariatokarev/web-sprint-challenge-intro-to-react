d2835946a157f8cc0674e2673e9a5ebb07ddb7e5
mariatokarev <144080022+mariatokarev@users.noreply.github.com>
2023-10-11 23:01:21 -0400

    Add files via upload

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,58 +3,54 @@ import axios from 'axios'
 import Character from './Character'
 
 
-const urlPlanets = 'http://localhost:9009/api/planets';
-const urlPeople = 'http://localhost:9009/api/people';
+
 
  function App() {
   const [peopleData, setPeopleData] = useState([]);
-  const [planetsData, setPlanetsData] = useState([]);
-  const [showHomeworld, setShowHomeworld] = useState(null);
+  const [ planetsData, setPlanetsData] = useState([]);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const characterResponse = await axios.get(urlPeople);
-        const planetResponse = await axios.get(urlPlanets);
+        const characterResponse = await axios.get('http://localhost:9009/api/people');
+        const planetResponse = await axios.get('http://localhost:9009/api/planets');
         setPeopleData(characterResponse.data);
         setPlanetsData(planetResponse.data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     }
-
     fetchData();
   }, []);
 
-  const openHomeworld = (urlPlanets) => {
-    setShowHomeworld(urlPlanets);
-  };
-
-  const closeHomeworld = () => {
-    setShowHomeworld(null);
+  const handleCharacterClick = (character) => {
+    if (selectedCharacter === character) {
+      setSelectedCharacter(null);
+    } else {
+      setSelectedCharacter(character);
+    }
   };
 
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {peopleData.map((person, index) => (
-        <div key={index} className="character-card">
-          <h3 className="character-name">{person.name}</h3>
-          <button onClick={() => openHomeworld(person.homeworld)}>
-            {person.name}'s Homeworld
-          </button>
-          {showHomeworld === person.homeworld && (
-            <div className="character-planet">
-            
-            </div>
-          )}
-        </div>
+      {peopleData.map((character) => (
+        <Character
+          key={character.id}
+          character={character}
+          planet={planetsData.find((planet) => planet.id === character.homeworld)}
+          onCharacterClick={handleCharacterClick}
+          isPlanetVisible={selectedCharacter === character}
+        />
       ))}
     </div>
   );
 }
-export default App
+
+export default App;
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
 if (typeof module !== 'undefined' && module.exports) module.exports = App
