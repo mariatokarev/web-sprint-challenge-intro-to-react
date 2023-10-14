f8ad55abf4fc1b1a0c4a0dee6f5bac18fbbca633
mariatokarev <144080022+mariatokarev@users.noreply.github.com>
2023-10-13 21:25:22 -0400

    Add files via upload

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -2,19 +2,39 @@ import React, {} from 'react';
 
 
 function Character(props) {
-    const { character, planet, onCharacterClick, isPlanetVisible } = props;
-  
-    return (
-      <div className="character-card" onClick={() => onCharacterClick(character)}>
-        <h3 className='character-name'>{character.name}</h3>
-        {isPlanetVisible && (
-          <p>
-            Planet:
-            <span className='character-planet'>{planet.name}</span>
-          </p>
-        )}
-      </div>
-    );
-  }
+   const [isSelected, setIsSelected] = useState(false);
+
+   const handleCardClick = () => {
+      if (!isSelected) {
+        setIsSelected(true);
+        onCharacterClick(character);
+      }
+    };
+ 
+   return (
+     <div
+       className={`character-cards ${isSelected ? 'selected' : ''}`}
+       onClick={handleCardClick}
+     >
+       <h3 className='character-name'>{character.name}</h3>
+      
+       {isSelected && (
+         <p>
+           Planet:
+           <span className='character-planet'>{planet.name}</span>
+         </p>
+       )}
+     </div>
+   );
+ }
+
+ 
+ 
+ 
+ 
+ 
+ 
+ Regenerate
+ 
   
   export default Character;
